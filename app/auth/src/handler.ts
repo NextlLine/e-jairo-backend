57a3d626522fdb4836e8c7ed1c67d0b019517227
Jairo Júnior <jairojjr2003@gmail.com>
2025-12-15 15:24:11 -0300

    feat: signup function

--- app/auth/src/handler.ts
+++ app/auth/src/handler.ts
@@ -1,22 +1,38 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { helloWorld } from "./service";
+import { signUpUserService } from "./service";
+import { ZodError } from "zod";
 
-export async function testHelloWorld(
+export async function signUpUser(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
   try {
-    const message = await helloWorld();
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Must pass a user" }),
+      };
+    }
+
+    const body = JSON.parse(event.body!);
+    const response = await signUpUserService(body);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message }),
+      body: JSON.stringify(response),
     };
-  } catch (error) {
+  } catch (err: any) {
+    console.error("Error in signUpUser handler:", err);
+
+    if (err instanceof ZodError) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Invalid input", errors: err.issues }),
+      };
+    }
+
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        message: "Internal Server Error",
-        error: error instanceof Error ? error.message : String(error),
-      }),
+      body: JSON.stringify({ message: "Internal server error" }),
     };
   }
 }
--- app/auth/src/service.ts
+++ app/auth/src/service.ts
@@ -1,3 +1,42 @@
-export async function helloWorld(): Promise<string> {
-  return "Hello, World!";
+import z from "zod";
+import {
+  CognitoIdentityProviderClient,
+  CognitoIdentityProviderClientConfig,
+  SignUpCommand
+} from "@aws-sdk/client-cognito-identity-provider";
+
+
+const config: CognitoIdentityProviderClientConfig = {
+  region: process.env.AWS_REGION || "us-east-1",
+
+};
+const client = new CognitoIdentityProviderClient(config);
+
+const SignUpUserSchema = z.object({
+  userName: z.string().min(3).max(30),
+  email: z.string().email(),
+  password: z.string().min(8).max(100),
+});
+
+export async function signUpUserService(userData: z.infer<typeof SignUpUserSchema>) {
+  const validatedData = SignUpUserSchema.parse(userData);
+
+  if (!process.env.COGNITO_CLIENT_ID) {
+    throw new Error("COGNITO_CLIENT_ID not configured");
+  }
+
+  const signUpCommand = new SignUpCommand({
+    ClientId: process.env.COGNITO_CLIENT_ID!,
+    Username: validatedData.userName,
+    Password: validatedData.password,
+    UserAttributes: [{ Name: "email", Value: validatedData.email },],
+  });
+
+  const response = await client.send(signUpCommand);
+
+  return {
+    userSub: response.UserSub,
+    userConfirmed: response.UserConfirmed,
+  };
+
 }
